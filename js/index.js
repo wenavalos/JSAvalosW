ead68d30cdf79f010fc8d19ca6d07e2b1e4c87de
Wendy Avalos <wendy_avalos@live.com>
2022-07-14 23:17:41 -0300

    Se agrego el codigo

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+let repetir= parseInt(prompt('Ingrese la cantidad de veces que quiere repetir el codigo'))
+
+
+for (let i= 0; i < repetir; i++) {
+    alert (`Hola por ${i} vez`);   
+}
+
+alert('La palabra se repitio: ' + repetir +' veces' )
